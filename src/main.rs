cf4d7188fe45b5959d232608532986999764b088
Eric Hagman <emhagman@gmail.com>
2023-12-03 22:57:38 -0500

    Add Christmas tree

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ mod utils;
 use std::time::Instant;
 
 fn main() {
+    let tree = include_bytes!("../data/tree.txt");
+    println!("\x1b[31m{}\x1b[0m", "AoC 2023");
+    println!("\x1b[32m{}\x1b[0m", String::from_utf8_lossy(tree));
     let before = Instant::now();
     println!(
         "day 1, part 1: {} in {:.2?}",
