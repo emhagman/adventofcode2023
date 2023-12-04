34fb9a06c4fa753a60ebff404b47b35428993fdc
Eric Hagman <emhagman@gmail.com>
2023-12-03 22:46:31 -0500

    Add estimated runtimes

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 <https://adventofcode.com/2023>
 
+## Hardware
+
+Macbook M1 2020 (aarch64), 16GB RAM
+
 ## Solutions
+
+NOTE: Times are an estimate, they're not run 1000x for more accurate averages.
 
 | Day | Part 1 | Part 2 | Difficulty |
 |---|---|---| --- |
-| 1 | ✅ | ✅ | Annoying |
-| 2 | ✅ | ✅ | Easy |
-| 3 | ✅ | ✅ | Easy |
+| 1 | ✅ 165.54µs | ✅ 1.96ms | Annoying |
+| 2 | ✅ 125.33µs | ✅ 108.21µs | Easy |
+| 3 | ✅ 9.87ms | ✅ 9.39ms | Easy |
--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,43 @@ mod day2;
 mod day3;
 mod utils;
 
+use std::time::Instant;
+
 fn main() {
+    let before = Instant::now();
     println!(
-        "day 1, part 1: {}",
-        day1::part1(&utils::read_entire_file("./data/day1.txt"))
+        "day 1, part 1: {} in {:.2?}",
+        day1::part1(&utils::read_entire_file("./data/day1.txt")),
+        before.elapsed()
     );
+    let before = Instant::now();
     println!(
-        "day 1, part 2: {}",
-        day1::part2(&utils::read_entire_file("./data/day1.txt"))
+        "day 1, part 2: {} in {:.2?}",
+        day1::part2(&utils::read_entire_file("./data/day1.txt")),
+        before.elapsed()
     );
+    let before = Instant::now();
     println!(
-        "day 2, part 1: {}",
-        day2::part1(&utils::read_entire_file("./data/day2.txt"))
+        "day 2, part 1: {} in {:.2?}",
+        day2::part1(&utils::read_entire_file("./data/day2.txt")),
+        before.elapsed()
     );
+    let before = Instant::now();
     println!(
-        "day 2, part 2: {}",
-        day2::part2(&utils::read_entire_file("./data/day2.txt"))
+        "day 2, part 2: {} in {:.2?}",
+        day2::part2(&utils::read_entire_file("./data/day2.txt")),
+        before.elapsed()
     );
+    let before = Instant::now();
     println!(
-        "day 3, part 1: {}",
-        day3::part1(&utils::read_entire_file("./data/day3.txt"))
+        "day 3, part 1: {} in {:.2?}",
+        day3::part1(&utils::read_entire_file("./data/day3.txt")),
+        before.elapsed()
     );
+    let before = Instant::now();
     println!(
-        "day 3, part 2: {}",
-        day3::part2(&utils::read_entire_file("./data/day3.txt"))
+        "day 3, part 2: {} in {:.2?}",
+        day3::part2(&utils::read_entire_file("./data/day3.txt")),
+        before.elapsed()
     );
 }
